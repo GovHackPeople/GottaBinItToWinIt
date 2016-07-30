718665e1f53cfeb988eda83a73f2096856969453
Matthew Kairys <mkairys@gmail.com>
2016-07-30 21:00:24 +1000

    Placeholder activities, fragments and slight theme adjustments

---
+++ android/app/src/main/java/org/govhack/gottabinittowinit/fragments/DashboardFragment.java
@@ -0,0 +1,27 @@
+package org.govhack.gottabinittowinit.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.govhack.gottabinittowinit.R;
+
+public class DashboardFragment extends Fragment {
+
+    public static final String PAGE_TITLE = "Dashboard";
+
+    public DashboardFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+
+}
