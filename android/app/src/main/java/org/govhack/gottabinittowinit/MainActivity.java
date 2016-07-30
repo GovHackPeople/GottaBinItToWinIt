c1a7e48b5690374d6d5cdf88881d7e482e6f76eb
Matthew Kairys <mkairys@gmail.com>
2016-07-30 21:01:35 +1000

    Updated MainActivity with new interface

---
+++ android/app/src/main/java/org/govhack/gottabinittowinit/MainActivity.java
@@ -0,0 +1,96 @@
+package org.govhack.gottabinittowinit;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import org.govhack.gottabinittowinit.fragments.DashboardFragment;
+import org.govhack.gottabinittowinit.fragments.GalleryFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
+
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupToolbar();
+        setupViewPager();
+        setupTabLayout();
+        setupFab();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void setupViewPager() {
+        mViewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        if (mViewPager != null) {
+            Adapter adapter = new Adapter(getSupportFragmentManager());
+            adapter.addFragment(new DashboardFragment(), "Dashboard");
+            adapter.addFragment(new GalleryFragment(), "Gallery");
+            mViewPager.setAdapter(adapter);
+        }
+    }
+
+    private void setupFab() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, AchievementsActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+
+    private void setupTabLayout() {
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mPageTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment f, String title) {
+            mFragments.add(f);
+            mPageTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mPageTitles.get(position);
+        }
+    }
+}
